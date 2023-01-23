fb2999ead67e50e01b0b90c0bf127506b71093eb
Rida Essalhi <ressalhi@e1r3p8.1337.ma>
2023-01-23 15:16:02 +0100

    cpp01 ex04 done

--- cpp_module01/ex04/main.cpp
+++ cpp_module01/ex04/main.cpp
@@ -6,12 +6,13 @@
 /*   By: ressalhi <ressalhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 17:05:23 by ressalhi          #+#    #+#             */
-/*   Updated: 2022/11/30 22:43:34 by ressalhi         ###   ########.fr       */
+/*   Updated: 2023/01/23 15:14:41 by ressalhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
 int main(int ac, char **av)
 {
@@ -20,12 +21,28 @@ int main(int ac, char **av)
     std::string str = av[1];
     std::ifstream file1;
     file1.open(str, std::ios::in);
+    if (!file1){
+        std::cout << "Open File Error\n";
+        return (0);
+    }
     std::ofstream file(str + ".replace");
     std::string s,s1,s2;
     s1 = av[2], s2 = av[3];
+    int s1len = s1.size();
+    int s2len = s2.size();
     while (std::getline(file1, s))
     {
-        s.replace(0, 5, s1);
+        int n = -2;
+        while (n != -1 && av[2][0] != '\0'){
+            if (n >= 0)
+                n = s.find(s1, n+s2len);
+            else if (n == -2)
+                n = s.find(s1);
+            if (n >= 0){
+                s.erase(n, s1len);
+                s.insert(n, s2);
+            }
+        }
         file << s;
         file << "\n";
     }
