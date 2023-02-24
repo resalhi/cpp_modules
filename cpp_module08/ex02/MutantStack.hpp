5df213d96e8124834676edbaab0f08ab8fdbe10a
Rida Essalhi <ressalhi@e1r8p7.1337.ma>
2023-02-24 22:14:00 +0100

    cpp08 is done

---
+++ cpp_module08/ex02/MutantStack.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ressalhi <ressalhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 20:47:20 by ressalhi          #+#    #+#             */
+/*   Updated: 2023/02/24 21:01:11 by ressalhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MutantStack_HPP
+# define MutantStack_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <stack>
+
+template <typename T>
+class MutantStack: public std::stack<T>
+{
+    public:
+        MutantStack(){}
+        MutantStack& operator=(MutantStack const &mutant){ *this->c = mutant.c;return (*this); }
+        MutantStack(MutantStack const &mutant){ this->operator=(mutant); }
+        ~MutantStack(){}
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin(void) {
+            return (this->c.begin());
+        }
+        iterator end(void) {
+            return (this->c.end());
+        }
+};
+
+#endif
---
+++ cpp_module08/ex02/main.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ressalhi <ressalhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 20:47:02 by ressalhi          #+#    #+#             */
+/*   Updated: 2023/02/24 20:57:01 by ressalhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << "first elem: " << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << "size: " << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::cout << "---------------\n";
+    std::stack<int> s(mstack);
+    while (!s.empty())
+    {
+        std::cout << s.top() << std::endl;
+        s.pop();
+    }
+    
+}
