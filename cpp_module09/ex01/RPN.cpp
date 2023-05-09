f09adc391b744480735385d83afa17efeb7c372f
Rida Essalhi <ressalhi@e2r10p8.1337.ma>
2023-05-09 22:00:26 +0100

    ex01 is 80% done

--- cpp_module09/ex01/RPN.cpp
+++ cpp_module09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: ressalhi <ressalhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 11:12:59 by ressalhi          #+#    #+#             */
-/*   Updated: 2023/05/09 14:19:50 by ressalhi         ###   ########.fr       */
+/*   Updated: 2023/05/09 21:53:35 by ressalhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,52 @@ bool    str_isdigit(std::string str){
 
 RPN::RPN(char *av){
     std::string str;
+    std::stack<long> tmp_num;
+    std::stack<char> tmp_tok;
     std::stringstream s(av);
     while (s >> str){
         if (str == "+" || str == "-" || str == "*" || str == "/")
-            this->tok.push(str[0]);
+            tmp_tok.push(str[0]);
         else if (str_isdigit(str)){
             long x = strtol(str.c_str(), NULL, 10);
             if (x > 10)
                 throw std::runtime_error("Error");
-            this->num.push(x);
+            tmp_num.push(x);
         }
         else
             throw std::runtime_error("Error");
     }
-    if (this->tok.size() != this->num.size()-1)
+    if (tmp_tok.size() != tmp_num.size()-1)
         throw std::runtime_error("Error");
+    while (!tmp_num.empty()){
+        this->num.push(tmp_num.top());
+        tmp_num.pop();
+    }
+    while (!tmp_tok.empty()){
+        this->tok.push(tmp_tok.top());
+        tmp_tok.pop();
+    }
+    long sum;
+    sum = this->num.top();
+    this->num.pop();
+    while (!this->num.empty()){
+        char t = this->tok.top();
+        long n = this->num.top();
+        this->tok.pop();
+        this->num.pop();
+        if (t == '+')
+            sum = sum + n;
+        else if (t == '*')
+            sum = sum * n;
+        else if (t == '-')
+            sum = sum - n;
+        else if (t == '/'){
+            if (!n)
+                throw std::runtime_error("Error");
+            sum = sum / n;
+        }
+    }
+    std::cout << sum << '\n';
 }
 
 RPN::~RPN(){}
