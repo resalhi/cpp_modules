a85469e76efffe4eb13f541f2f16944921a99f6f
Rida Essalhi <ressalhi@e1r9p11.1337.ma>
2023-05-10 14:15:31 +0100

    ex01 cpp9 is done

--- cpp_module09/ex01/RPN.cpp
+++ cpp_module09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: ressalhi <ressalhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 11:12:59 by ressalhi          #+#    #+#             */
-/*   Updated: 2023/05/09 21:53:35 by ressalhi         ###   ########.fr       */
+/*   Updated: 2023/05/10 14:14:51 by ressalhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,52 +24,57 @@ bool    str_isdigit(std::string str){
 
 RPN::RPN(char *av){
     std::string str;
-    std::stack<long> tmp_num;
-    std::stack<char> tmp_tok;
     std::stringstream s(av);
     while (s >> str){
-        if (str == "+" || str == "-" || str == "*" || str == "/")
-            tmp_tok.push(str[0]);
+        if (str == "+"){
+            long n2 = this->num.top();
+            this->num.pop();
+            long n1 = this->num.top();
+            this->num.pop();
+            this->num.push(n1+n2);
+        }
+        else if (str == "-"){
+            long n2 = this->num.top();
+            this->num.pop();
+            long n1 = this->num.top();
+            this->num.pop();
+            this->num.push(n1-n2);
+        }
+        else if (str == "*"){
+            long n2 = this->num.top();
+            this->num.pop();
+            long n1 = this->num.top();
+            this->num.pop();
+            this->num.push(n1*n2);
+        }
+        else if (str == "/"){
+            long n2 = this->num.top();
+            this->num.pop();
+            long n1 = this->num.top();
+            this->num.pop();
+            if (!n2)
+                throw std::runtime_error("Error");
+            this->num.push(n1/n2);
+        }
         else if (str_isdigit(str)){
             long x = strtol(str.c_str(), NULL, 10);
-            if (x > 10)
-                throw std::runtime_error("Error");
-            tmp_num.push(x);
+            if (x > 10){
+                for (size_t i=0;i < str.size();i++){
+                    std::string c;
+                    c += str[i];
+                    long n = strtol(c.c_str(), NULL, 10);
+                    this->num.push(n);
+                }
+            }
+            else
+                this->num.push(x);
         }
         else
             throw std::runtime_error("Error");
     }
-    if (tmp_tok.size() != tmp_num.size()-1)
+    if (this->num.size() != 1)
         throw std::runtime_error("Error");
-    while (!tmp_num.empty()){
-        this->num.push(tmp_num.top());
-        tmp_num.pop();
-    }
-    while (!tmp_tok.empty()){
-        this->tok.push(tmp_tok.top());
-        tmp_tok.pop();
-    }
-    long sum;
-    sum = this->num.top();
-    this->num.pop();
-    while (!this->num.empty()){
-        char t = this->tok.top();
-        long n = this->num.top();
-        this->tok.pop();
-        this->num.pop();
-        if (t == '+')
-            sum = sum + n;
-        else if (t == '*')
-            sum = sum * n;
-        else if (t == '-')
-            sum = sum - n;
-        else if (t == '/'){
-            if (!n)
-                throw std::runtime_error("Error");
-            sum = sum / n;
-        }
-    }
-    std::cout << sum << '\n';
+    std::cout << this->num.top() << '\n';
 }
 
 RPN::~RPN(){}
